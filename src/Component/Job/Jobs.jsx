488b0dc351283da63637e5f4218e7f2ae68d5336
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2024-11-28 22:18:51 +0600

    sd

--- src/Component/Job/Jobs.jsx
+++ src/Component/Job/Jobs.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { IoLocationOutline } from "react-icons/io5";
+import { FiDollarSign } from "react-icons/fi";
 
 const Jobs = ({job}) => {
 
@@ -8,22 +10,24 @@ const Jobs = ({job}) => {
     return (
         <div className='border rounded-md p-10'>
             <img src={logo} alt={company_name} />
-            <h1>{job_title}</h1>
-            <h4>{company_name}</h4>
-            <div className='flex gap-6'>
+            <h1 className='font-bold text-xl my-2'>{job_title}</h1>
+            <h4 className='font-medium text-lg'>{company_name}</h4>
+            <div className='flex gap-6 my-2'>
                 <span className='border border-[#4897FE] p-2 rounded-md'>{remote_or_onsite}</span>
                 <span className='border border-[#4897FE] p-2 rounded-md'>{job_type}</span>
             </div>
             <div className='flex gap-5'>
-                <div>
+                <div className='flex'>
+                    <IoLocationOutline className='mt-1'/>
                     <span> {location}</span>
                 </div>
-                <div>
+                <div className='flex'>
+                <FiDollarSign className='mt-1'/>
                     <span> Salary:{salary}</span>
                 </div>
             </div>
            <Link to={`/job/${id}`}>
-           <button className='btn'>View Details</button>
+           <button className='btn my-2'>View Details</button>
            </Link>
         </div>
     );
--- src/Component/JobDetails/JobDetails.jsx
+++ src/Component/JobDetails/JobDetails.jsx
@@ -1,16 +1,47 @@
 import { useLoaderData, useParams } from "react-router-dom";
-
+import { FiDollarSign } from "react-icons/fi";
+import { MdDateRange } from "react-icons/md";
+import { MdLocationPin } from "react-icons/md";
+import { IoMailOutline } from "react-icons/io5";
+import { FaPhone } from "react-icons/fa";
 
 const JobDetails = () => {
     const job=useLoaderData();
     const {id}=useParams();
     const jobs=job.find(job => job.id == id);
-    const {job_title}=jobs;
+    const {job_title,job_description,job_responsibility,
+        educational_requirements,experiences,salary,contact_information}=jobs;
+    
+    const {email,phone,address}=contact_information;
     
     return (
-        <div>
-            <h1>Job Details of:{id}</h1>
-            <p>{job_title}</p>
+        <div className="flex gap-10 w-[85%] m-auto my-16">
+            <div className="w-[60%] ">
+             <p className="pb-3"><span className="text-lg font-medium ">Job Description:</span>{job_description}</p>
+             <p><span className="text-lg font-medium py-3">Job Responsibility:</span>{job_responsibility}</p>
+             <p className="py-3">
+             <span className="text-lg font-medium">Educational Requirements:</span>
+             <span>{educational_requirements}</span>
+             </p>
+             <p className="py-3">
+                <span className="text-lg font-medium ">Experiences:</span>
+                <span>{experiences}</span>
+             </p>
+            </div>
+            <div className="w-[40%] ">
+            <div className="bg-[#F4F2FF] p-6 rounded-lg">
+            <h1 className="text-xl font-bold">Job Details</h1>
+            <hr />
+            <p className="flex gap-2 my-2"><FiDollarSign className='mt-1'/><span>Salary : {salary} (Per Month)</span></p>
+            <p className="flex gap-2 my-2"><MdDateRange className='mt-1'/> <span>Job Title :{job_title}</span></p>
+            
+            <p className="text-xl font-bold">Contact Information</p>
+            <hr />
+            <p className="flex gap-2 my-2"><FaPhone className='mt-1'/><span>Phone :{phone}</span> </p>
+            <p className="flex gap-2 my-2"><IoMailOutline className='mt-1'/><span>Email :{email}</span> </p>
+            <p className="flex gap-2 my-2"><MdLocationPin className='mt-1'/><span>Address :{address}</span> </p>
+            </div>
+            </div>
         </div>
     );
 };
